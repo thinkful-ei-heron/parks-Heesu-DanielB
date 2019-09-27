4b78895f624ecb9052f8eacb1816cd01ffd816ad
Daniel Bright <brahyt@gmail.com>
2019-09-27 15:53:00 -0700

    done

--- index.js
+++ index.js
@@ -1,37 +1,33 @@
 'use strict'
 
 // put your own value below!
-const apiKey = ''; 
-const searchURL = '';
+const apiKey = 'SWkWIODVpc0Ecz3sIzHo9dm88Jd4ZN1oMMus4WBL'; 
+const searchURL = 'https://developer.nps.gov/api/v1/parks';
 
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
   return queryItems.join('&');
 };
-
+//Full Name of park(fullName) - description(description) - website URL(url)
 function displayResults(responseJson) {
     // if there are previous results, remove them
-    console.log(responseJson);
-
+    let arrayState = responseJson.data;
     $('#results-list').empty();
-    // iterate through the items array
-    for (let i = 0; i < responseJson.data.length; i++){
-      $('#results-list').append(
+  arrayState.forEach(x => $('#results-list').append(
         `<li>
-            <h3>${responseJson.data[i].snippet.title}</h3>
-            <p>${responseJson.data[i].snippet.description}</p>
-            <a href='${responseJson.data[i].snippet.thumbnails.default.url}'>
+            <h3>${x.fullName}</h3>
+            <p>${x.description}</p>
+            <a href='${x.url}'>Link</a>
         </li>`
-      )};
-
+      ))
     $('#results').removeClass('hidden');
 };
 
 function getParkInfo(query, maxResults) {
     const params = {
       api_key: apiKey,
-      q: query,
+      stateCode: query,
       limit: maxResults
     };
 
@@ -55,7 +51,7 @@ function getParkInfo(query, maxResults) {
 function watchForm() {
     $('form').submit(event => {
       event.preventDefault();
-      const searchTerm = $('#js-search-term').val();
+      const searchTerm = $('#js-search-states').val();
       const maxResults = $('#js-max-results').val();
       getParkInfo(searchTerm, maxResults);
     });
